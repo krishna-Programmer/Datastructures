3b9d8ba0501873748fa407451de149d736578af7
krishna <krishnarao@smartinfra.co.in>
2017-08-24 09:03:30 +0530

    3

--- LinkedList.java
+++ LinkedList.java
@@ -4,12 +4,12 @@ public class  LinkedList<T>
 Node<T> head;
 int length;
 Node<T> previousNode;
-Node<T> currentNodeNode;
-Node<T> nexttNode;
+Node<T> currentNode;
+Node<T> nextNode;
 int index;
 private static class Node<T>
 {
-Node<T> nexttNode;
+Node<T> next;
 T data;
 Node(T data)
 {
@@ -25,29 +25,29 @@ public void add(T data)
 if(head==null)head=new Node<T>(data);
 else
 {
-currentNodeNode=head;
-while(currentNodeNode.nexttNode!=null)currentNodeNode=currentNodeNode.nexttNode;
-currentNodeNode.nexttNode=new Node<T>(data);
+currentNode=head;
+while(currentNode.next!=null)currentNode=currentNode.next;
+currentNode.next=new Node<T>(data);
 }
 ++length;
 }
 public void remove(T data)
 {
-currentNodeNode=head;
+currentNode=head;
 previousNode=null;
 int index=0;
 boolean found=false;
-while(currentNodeNode!=null){
+while(currentNode!=null){
 ++index;
-if(currentNodeNode.data==data){found=true;break;};
-previousNode=currentNodeNode;currentNodeNode=currentNodeNode.nexttNode;
+if(currentNode.data==data){found=true;break;};
+previousNode=currentNode;currentNode=currentNode.next;
 }
 
 if(found==true)
 {
 System.out.println("Element found at"+index);
-if(currentNodeNode==head)head=currentNodeNode.nexttNode;
-else previousNode.nexttNode=currentNodeNode.nexttNode;
+if(currentNode==head)head=currentNode.next;
+else previousNode.next=currentNode.next;
 --length;
 }
 else
@@ -57,21 +57,21 @@ System.out.println("Element not found");
 }
 public void print()
 {
-  currentNodeNode=head;
-  while(currentNodeNode!=null){System.out.println(currentNodeNode.data);currentNodeNode=currentNodeNode.nexttNode;}
+  currentNode=head;
+  while(currentNode!=null){System.out.println(currentNode.data);currentNode=currentNode.next;}
 }
 public void addBeforeHead(T data)
 {
  Node<T> temp=head;
  head=new Node<T>(data);
- head.nexttNode=temp.nexttNode;
+ head.next=temp.next;
 }
 public void insertAfter(T data)
 {
  find(T data);
 
- currentNodeNode.nexttNode=new Node<T>(data);
- currentNodeNode.nexttNode.nexttNode=currentNodeNode;
+ currentNode.next=new Node<T>(data);
+ currentNode.next.next=currentNode;
 }
 public void insertBefore(T data)
 {
@@ -80,21 +80,21 @@ public void insertBefore(T data)
 }
 public void find(T data) 
 {
-currentNodeNode=head;
+currentNode=head;
 previousNode=null;
 int index=0;
 boolean found=false;
-while(currentNodeNode!=null){
+while(currentNode!=null){
 ++index;
-if(currentNodeNode.data==data){found=true;break;};
-previousNode=currentNodeNode;currentNodeNode=currentNodeNode.nexttNode;
+if(currentNode.data==data){found=true;break;};
+previousNode=currentNode;currentNode=currentNode.next;
 }
 
 if(found==true)
 {
 System.out.println("Element found at"+index);
-if(currentNodeNode==head)head=currentNodeNode.nexttNode;
-else previousNode.nexttNode=currentNodeNode.nexttNode;
+if(currentNode==head)head=currentNode.next;
+else previousNode.next=currentNode.next;
 --length;
 }
 else
